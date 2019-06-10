223a01f48621260c4225ea1032dd7877fe5ecb3a
vincenterr <chung.menghsun@gmail.com>
2019-06-10 22:22:06 +0800

    Modify home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
 import PageWrapper from '../components/page-wrapper'
+import P5Wrapper from '../components/p5-wrapper'
+import gameOfLifeSketch from '../p5-sketches/game-of-life'
 
-export default PageWrapper(() => <div>Conway's game of life!</div>)
+function HomePage() {
+  return (
+    <Wrapper>
+      <P5Wrapper sketch={gameOfLifeSketch} />
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div``
+
+export default PageWrapper(HomePage)
