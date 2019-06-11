fd0c45bc9912c0fcf100be63b7a1b3df8e2825a8
vincenterr <chung.menghsun@gmail.com>
2019-06-11 12:52:04 +0800

    Add icons

---
+++ src/components/icons.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { MdRefresh, MdPlayArrow, MdPause, MdStop } from 'react-icons/md'
+
+export const Refresh = props => <MdRefresh {...props} />
+export const Play = props => <MdPlayArrow {...props} />
+export const Pause = props => <MdPause {...props} />
+export const Stop = props => <MdStop {...props} />
